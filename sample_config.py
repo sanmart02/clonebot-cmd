75e8001186715eadeedd26a335f4fcce473234d5
sanmart02 <113845440+sanmart02@users.noreply.github.com>
2023-04-04 19:58:32 -0300

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -30,7 +30,7 @@ class Config(object):
     API_HASH = "7dd9974c713787410beae4a295cc1e2d"
 
     # Generate a user session string
-    TG_USER_SESSION = 
+    TG_USER_SESSION = "AQAAzB4kZkggWqtCxwkQtOA6sUjnDWr-SKjHdm1mwrJIJBVEW2hawVirR3GBn-mvpbt3FRbgUTYQGMNQ4qtvztdLjl57M2lKTAbxwP91a_nq2Lsa2oyRLp6jigtCuz5ZdYpzS9s8--W6ljalJeT6vU_n-iYqmlKcKmZ-pKR1E5tM_jwVaHTdgj9y6CyKIyXBe8h0PUJ4mJ5axKIjR4NcBfPO_iIRSo6x1jnVrr01u5FlY2MGDboUf8tqzrhXfFxp1Ps4QnNrmGoHliHAnKRYACp423utR_-4epSCTRf_jUJICs7Y3gS0HLxgl1qBUphfzFIO145GCeNxHgrswT90AqtwAAAAAVGSwuQA"
 
 
 def LOGGER(name: str) -> logging.Logger:
